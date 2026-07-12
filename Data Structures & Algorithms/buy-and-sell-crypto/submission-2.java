4f5f56fa88769173e576feffbcd99707bb9b9860
bhavyasri08 <ettireddybhavyasri08@gmail.com>
2026-07-12 12:59:23 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,59 @@
+class Solution {
+    // public int maxProfit(int[] prices) {
+        
+    //     int longest=0;
+    //       int n=prices.length;
+    //      int[] suf=new int[n];
+    //       suf[n-1]=prices[n-1];
+       
+    //     for(int i=0;i<=n-2;i++){
+    //          int max=0;
+    //         for(int j=i;j<=n-1;j++){
+    //            if(max<=prices[j]){
+    //                   max=prices[j];
+    //            }
+              
+    //         }
+            
+    //          longest=Math.max(longest,max-prices[i]);
+    //     }
+
+
+    //     return  longest;
+    // }
+    //  public int maxProfit(int[] prices) {
+        
+    //     int longest=0;
+    //       int n=prices.length;
+    //      int[] suf=new int[n];
+    //       suf[n-1]=prices[n-1];
+    //    for(int i=n-2;i>=0;i--){
+    //       suf[i]=Math.max(suf[i+1],prices[i]);
+    //    }
+    //    int max=0;
+    //     for(int i=0;i<=n-1;i++){
+    //         int dif=suf[i]-prices[i];
+    //         max= Math.max(dif,max);
+       
+
+    //     }
+    //     return max;
+      
+    // }
+     public int maxProfit(int[] prices) {
+        
+        int max=0;
+        int profit=0;
+        int i=0;
+        for(int j=1;j<prices.length;j++){
+            profit=prices[j]-prices[i];
+            max=Math.max(max,profit);
+     
+            if(prices[i]>prices[j]){
+                i=j;
+            }
+        }
+        return max;
+      
+    }
+}
